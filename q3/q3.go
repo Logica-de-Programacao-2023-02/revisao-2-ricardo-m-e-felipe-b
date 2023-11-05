aea83b0c503745bc1cfbece76d6a0414af7fa3bc
Felipe Barcelos de Carvalho <130105755+felpsbc@users.noreply.github.com>
2023-11-05 20:47:27 -0300

    Update q3.go

--- q3/q3.go
+++ q3/q3.go
@@ -1,5 +1,3 @@
-package q3
-
 //O cozinheiro Remy preparou uma refeição para si mesmo e, enquanto almoçava, decidiu assistir a um vídeo no RataTube. No
 //entanto, ele tem um tempo limitado de 𝑡 segundos para o almoço. Ele pediu a sua ajuda para escolher o vídeo ideal.
 //
@@ -11,12 +9,53 @@ package q3
 //houver vários vídeos que se encaixam nessa condição, ele deseja escolher o vídeo com o maior nível de entretenimento.
 //Retorne qualquer vídeo apropriado ou exiba um erro caso não haja um vídeo adequado dentro do tempo disponível.
 
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
 type Video struct {
-	ID            int
+	Nome          string
 	Duration      int
 	Entertainment int
 }
 
-func ChooseVideo(videos []Video, time int) (Video, error) {
-	return Video{}, nil
+func EscolherVideo(time int, videos []Video) (Video, error) {
+	var (
+		maxEntertainmentVideo Video
+	)
+	for i := range videos {
+		video := videos[i]
+		if video.Duration <= time && video.Entertainment > maxEntertainmentVideo.Entertainment {
+			maxEntertainmentVideo = video
+		}
+		time--
+	}
+	if maxEntertainmentVideo.Entertainment == 0 {
+		return Video{}, errors.New("nenhum video encontrado")
+	}
+	return maxEntertainmentVideo, nil
+}
+
+func main() {
+	var t int
+	fmt.Println("Quanto tempo você possui de almoço(em segundos) ? ")
+	fmt.Scanln(&t)
+
+	videos := []Video{
+		{"Vídeo 1", 120, 5},
+		{"Vídeo 2", 90, 4},
+		{"Vídeo 3", 180, 7},
+		{"Vídeo 4", 60, 3},
+	}
+
+	videoEscolhido, err := EscolherVideo(t, videos)
+
+	if err != nil {
+		fmt.Println("Erro:", err)
+	} else {
+		fmt.Printf("Vídeo escolhido: %s\n", videoEscolhido.Nome)
+	}
 }
