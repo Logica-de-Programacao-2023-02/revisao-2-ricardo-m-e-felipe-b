48c87607b3d97542f8e1b1f5eb1706ade792356d
Felipe Barcelos de Carvalho <130105755+felpsbc@users.noreply.github.com>
2023-11-05 22:35:45 -0300

    Update q5.go

--- q5/q5.go
+++ q5/q5.go
@@ -1,5 +1,3 @@
-package q5
-
 //Um novo serviço de e-mail, chamado "CEUBdesk", será inaugurado no CEUB em um futuro próximo. A administração do
 //site quer lançar o projeto o mais rápido possível, por isso eles pedem a sua ajuda. Você é sugerido(a) a implementar o
 //protótipo do sistema de registro do site. O sistema deve funcionar com o seguinte princípio.
@@ -11,6 +9,47 @@ package q5
 //seguinte regra. Números, começando com 1, são anexados um após o outro ao nome (name1, name2, ...), entre esses números,
 //o menor `i` é encontrado de forma que namei ainda não exista no banco de dados.
 
-func Register(names []string) []string {
-	return nil
+package main
+
+import (
+	"fmt"
+)
+
+type SistemaDeRegistro struct {
+	usuarios map[string]bool
+}
+
+func NovoRegistro() *SistemaDeRegistro {
+	return &SistemaDeRegistro{
+		usuarios: make(map[string]bool),
+	}
+}
+
+func (s *SistemaDeRegistro) Registrar(nome string) string {
+	if s.usuarios[nome] {
+		novoNome := nome
+		i := 1
+		for {
+			novoNome = fmt.Sprintf("%s%d", nome, i)
+			if !s.usuarios[novoNome] {
+				s.usuarios[novoNome] = true
+				return novoNome
+			}
+			i++
+		}
+	} else {
+		s.usuarios[nome] = true
+		return "OK"
+	}
+}
+
+func main() {
+	sistema := NovoRegistro()
+
+	fmt.Println(sistema.Registrar("Alice"))
+	fmt.Println(sistema.Registrar("Bob"))
+	fmt.Println(sistema.Registrar("Alice"))
+	fmt.Println(sistema.Registrar("Bob"))
+	fmt.Println(sistema.Registrar("Charlie"))
+	fmt.Println(sistema.Registrar("Alice"))
 }
